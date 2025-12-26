f5feee2e2105ca5216f1848e318abc69d8db7004
shuvo2o <eng.shuvo03@gmail.com>
2025-12-26 11:00:59 +0600

    counter changes

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -7,7 +7,7 @@ const Counter = ({count, handleIncrement, handleDecrement}) => {
         <h2 className='mb-4 text-2xl font-bold'>Counter App</h2>
         <p className='mb-8 text-lg font-medium'>{count}</p>
         <div className='space-x-2'>
-            <button onClick={handleIncrement} className='px-6 py-2 text-white bg-blue-500 rounded'>Increase (+)</button>
+            <button onClick={handleIncrement} className='px-6 py-2 text-white bg-green-500 rounded'>Increase (+)</button>
             <button onClick={handleDecrement} className='px-6 py-2 text-white bg-red-500 rounded'>Decrease (-)</button>
         </div>
     </div>
